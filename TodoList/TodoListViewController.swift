829739c51498f55127fe125833d2e51395174a8b
Nilson Nascimento <nilson.nascimento@lavu.com>
2018-09-02 21:56:38 -0400

    GH-1 - Add Item object and update TodoList data source

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    var todos = [Item(title: "Milk"),
+                 Item(title: "Juice"),
+                 Item(title: "Bread", isComplete: true)]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -10,12 +14,18 @@ class TodoListViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return todos.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        cell.textLabel?.textColor = todo.isComplete ? .gray : .black
+
         return cell
     }
 
