e6f59fc4354d955871b991ffd5e3fd1b40b3cf2f
Azarov Alex <alex@azarov.by>
2018-10-07 23:54:48 +0300

    GH-11 - App shortcut added.
    "Clear all" shortcut removes all todos nilling property in UserDefaults.

--- TodoList/AppDelegate.swift
+++ TodoList/AppDelegate.swift
@@ -16,5 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        completionHandler(handleShortcut(shortcutItem))
+    }
+
+    func handleShortcut(_ shortcutItem: UIApplicationShortcutItem ) -> Bool {
+        print("Handling shortcut")
+        
+        var succeeded = false
+        
+        if (shortcutItem.type == "io.chuva.TodoList.clear-all") {
+            UserDefaults.standard.set(nil, forKey: "todoList")
+            let navController = UINavigationController(rootViewController: TodoListViewController(style: .grouped))
+            window?.rootViewController = navController
+            window?.makeKeyAndVisible()
+            succeeded = true
+        }
+        
+        return succeeded
+        
+    }
 }
 
