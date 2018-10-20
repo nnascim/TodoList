0140f00daf1e57c60bc087c1c19ea040b2221469
ryantstone <ryan@ryanstone.io>
2018-10-20 16:25:49 -0400

    Refactor Item
    Set item up so that talk time isn’t used up discussing codable

--- TodoList/Models/Item.swift
+++ TodoList/Models/Item.swift
@@ -1,11 +1,63 @@
 import Foundation
+import CloudKit
 
-struct Item: Codable {
+class Item: CKNamed {
+    var ckName = "item"
     let title: String
-    var isComplete: Bool
-    
-    init(title: String, isComplete: Bool = false) {
-        self.title = title
+    var record: CKRecord?
+    var recordName: String?
+    var isComplete: Bool {
+        didSet { serialize() }
+    }
+
+    init(title: String, isComplete: Bool = false, record: CKRecord?) {
+        self.title      = title
         self.isComplete = isComplete
+        self.record     = record
+        serialize()
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container   = try decoder.container(keyedBy: CodingKeys.self)
+        recordName      = try container.decode(String.self, forKey: .recordName)
+        title           = try container.decode(String.self, forKey: .title)
+        isComplete      = try container.decode(Bool.self, forKey: .isComplete)
+    }
+}
+
+// MARK: - Serialization
+extension Item: Serializable {
+    func serialize() {
+        CloudKitService.init().save(record: buildRecord(), success: { (record) in
+            self.record = record
+        }, failure: {(error) in
+            print(error)
+        })
+    }
+
+    func buildRecord() -> CKRecord {
+        let record = self.record == nil ? CloudKitService.init().blankRecord(type: self) : self.record!
+        recordName              = record.recordID.recordName
+        record["isComplete"]    = isComplete ? 0 : 1
+        record["title"]         = title
+        return record
+    }
+}
+
+// MARK: - Codable
+extension Item: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case title,
+        isComplete,
+        recordName
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(title, forKey: .title)
+        try container.encode(isComplete, forKey: .isComplete)
+        if let recordName = self.recordName {
+            try container.encode(recordName, forKey: .recordName)
+        }
     }
 }
