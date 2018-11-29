6edbc75e205639ade6f5da9be1c9bf9b69f6b029
Jim Slusser <jslusser@gmail.com>
2018-11-29 16:23:03 -0500

    Initial commit

--- TodoList/Extensions/NSObjectExtensions.swift
+++ TodoList/Extensions/NSObjectExtensions.swift
@@ -2,6 +2,7 @@ import Foundation
 
 // Extension: To avoid typing string as it is prone to error. EX: TodoListViewController.className
 extension NSObject {
+    /// <#Description#>
     var className: String {
         return String(describing: type(of: self))
     }
