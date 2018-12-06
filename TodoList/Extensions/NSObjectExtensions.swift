b51592a11d4ada76bf8e32d64693ceb9a67546bb
gionoa <gnoa001@fiu.edu>
2018-12-06 14:13:39 -0500

    className documentation

--- TodoList/Extensions/NSObjectExtensions.swift
+++ TodoList/Extensions/NSObjectExtensions.swift
@@ -1,13 +1,25 @@
 import Foundation
 
 extension NSObject {
-    /*asddasasdas
-     
- */
+    
+    /// Convenience variable for obtaining the name of an object's class.
+    ///
+    /// Use className instead of:
+    ///
+    ///     object.String(describing: type(of: self))
+    ///
+    ///
+    /// - Returns: String containing the name of the object's class.
     var className: String {
+        
         return String(describing: type(of: self))
     }
     
+    /// Static class variable for obtaining a class' name.
+    ///
+    /// Use className instead of:
+    ///
+    ///     class.String(describing: type(of: self))
     static var className: String {
         return String(describing: self)
     }
