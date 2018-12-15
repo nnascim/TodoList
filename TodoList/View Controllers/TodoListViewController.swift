840aaa5fc71d30825825a02a3fa6fbbc094df331
Jim Slusser <jslusser@gmail.com>
2018-12-15 15:19:36 -0500

    Exploring Table Swipe Actions

--- TodoList/View Controllers/TodoListViewController.swift
+++ TodoList/View Controllers/TodoListViewController.swift
@@ -155,3 +155,34 @@ extension TodoListViewController: TextFieldViewDelegate {
         tableViewController.add(item: item)
     }
 }
+
+// Gio - I want to add the ability to display custom actions when the user swipes a table row,
+// specifically the ability to edit.  The example code below is from
+// https://useyourloaf.com/blog/table-swipe-actions/ where they give the ability to
+// 'favorite' an item.  We'll change this to Edit, but just for starters, do I even have this code in the right place?  And if so, it seems we need to replace dataSource? with something else, but I can't figure out what that it.  Thoughts?
+
+func tableView(_ tableView: UITableView,
+                        leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath)
+    ->   UISwipeActionsConfiguration? {
+        
+        // Get current state from data source
+        guard let favorite = dataSource?.favorite(at: indexPath) else {
+            return nil
+        }
+        
+        let title = favorite ?
+            NSLocalizedString("Unfavorite", comment: "Unfavorite") :
+            NSLocalizedString("Favorite", comment: "Favorite")
+        
+        let action = UIContextualAction(style: .normal, title: title,
+                                        handler: { (action, view, completionHandler) in
+                                            // Update data source when user taps action
+                                            self.dataSource?.setFavorite(!favorite, at: indexPath)
+                                            completionHandler(true)
+        })
+        
+        action.image = UIImage(named: "heart")
+        action.backgroundColor = favorite ? .red : .green
+        let configuration = UISwipeActionsConfiguration(actions: [action])
+        return configuration
+}
